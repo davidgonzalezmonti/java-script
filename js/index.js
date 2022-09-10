a7b573eff15538a0374b4068c2561763c1d65a7a
David Gonzalez Monti <107081679+davidgonzalezmonti@users.noreply.github.com>
2022-09-09 21:34:53 -0300

    entregaAgregandoDom

--- js/index.js
+++ js/index.js
@@ -14,93 +14,8 @@ const THIRTY = 30;
 const TWENTY = 20;
 const FIFTEEN = 15;
 
-const formasPago = [{
-    cuotas:   12,
-    interes: THIRTY
-},
-{
-    cuotas:   6,
-    interes: TWENTY
-},
-{
-    cuotas:   3,
-    interes: FIFTEEN
-},
-{
-    cuotas:   1,
-    interes: 1
-}
-];
+const formasPago = [{cuotas: 12, interes: THIRTY}, {cuotas: 6, interes: TWENTY}, {cuotas: 3, interes: FIFTEEN}, { cuotas: 1, interes: 1}];
 
-
-function compraStickers () {
-do {
-    opcion = Number (prompt("SELECCIONE SUS STICKERS:\n 10. Logo Mugiwara \n 9. Luffy \n 8. Zoro \n 7. Goku \n 6. Vegeta \n 5. Kid Buu \n 4. Tanjiro \n 3. Inosuke \n 2. Zenitsu \n 1. IR AL CARRITO \n 0. SALIR"));
-    switch (opcion) {
-        case 10:
-            arrayDeStickers.push(mugiwara);
-                alert("Mugiwara se añado al carrito")
-            break
-        case 9:
-            arrayDeStickers.push(luffy);
-            alert("Luffy se añado al carrito")
-
-            break
-
-        case 8:
-            arrayDeStickers.push(zoro);
-            alert("Zoro se añado al carrito")
-
-            break
-        
-            case 7:
-            arrayDeStickers.push(goku);
-            alert("Goku se añado al carrito")
-
-            break
-
-            case 6:
-            arrayDeStickers.push(vegeta);
-            alert("Vegeta se añado al carrito")
-
-            break
-
-            case 5:
-            arrayDeStickers.push(kidBuu);
-            alert("Kid Buu se añado al carrito")
-
-            break
-
-            case 4:
-            arrayDeStickers.push(tanjiro);
-            alert("Tanjiro se añado al carrito")
-
-            break
-
-            case 3:
-            arrayDeStickers.push(inosuke);
-            alert("Inosuke se añado al carrito")
-
-            break
-        
-            case 2:
-            arrayDeStickers.push(zenitsu);
-            alert("Zenitsu se añado al carrito")
-
-            break
-        
-            case 1:
-            menuCarrito();
-            break
-            
-        case 0:
-            break
-        default:
-            alert("Seleccione una opcion correcta");
-            break;
-    }
-} while (opcion !== 0 || opcion === null);
-}
 
 const cuotas = (total, interes) => {
     var porcentaje = interes / 100;
@@ -109,16 +24,9 @@ const cuotas = (total, interes) => {
             return {total: precioTotal, interes: interes};
 }
 
-function calculoCuotas () {
-    total = arrayDeStickers.reduce ((articulo, precios) => articulo + precios.precio, 0);
-                console.log("Total a pagar sin interes: $" + total);
-                formasPago.forEach(x => {
-                    console.log("Total con " + x.cuotas + " cuotas: $" + cuotas(total, x.interes).total);
-                    console.log("En " + x.cuotas + " cuotas de $" + cuotas(total, x.interes).total / x.cuotas + " con un interes de $" + cuotas(total, x.interes).interes);
-                });
-}
 
 function precioFinal(cant, interes) {
+    let contenedor = document.querySelector("#contenedor")
     const total = arrayDeStickers.reduce ((articulo, precios) => articulo + precios.precio, 0);
     const resultado = document.createElement("div")
         carritoDom();
@@ -126,96 +34,19 @@ function precioFinal(cant, interes) {
 
     if(cant > 1){
         resultado.innerHTML = `<h3>Total a pagar con ${cant} cuotas: $ ${cuotas(total, interes).total}</h3>
-                               <p>En ${cant} cuotas de $ ${cuotas(total, interes).total / cant} con un interes de $ ${cuotas(total, interes).interes}</p> `
+                               <p>En ${cant} cuotas de $ ${cuotas(total, interes).total / cant} con un interes de $ ${cuotas(total, interes).interes}</p> 
+                               <h5>¡Gracias por tu compra!</h5>`
     contenedor.append(resultado)
     } else {
-            resultado.innerHTML = `<h3>Total a pagar: $ ${total}</h3>`
+            resultado.innerHTML = `<h3>Total a pagar: $ ${total}</h3>
+                                    <h5>¡Gracias por tu compra!</h5>`
             contenedor.append(resultado)
         }
-
-        resultado.innerHTML= `<h5>¡Gracias por tu compra!</h5>`
-        contenedor.append(resultado)
     
 }
-
-
-
-function eliminarCarrito () { 
-    console.clear();
-    console.table(arrayDeStickers);
-    index = Number(prompt("Que producto desea eliminar?"));
-    arrayDeStickers.splice(index, 1);
-    alert("Producto eliminado")
-    console.clear();
-    console.table(arrayDeStickers);
-    
-}
-
-function menuCuotas() {
-    do {
-        valor = Number (prompt("En cuantas cuotas lo queres pagar? \n 12 cuotas con recargo del 30% \n 6 cuotas con recargo del 20% \n 3 cuotas con recargo del 10% \n 1 cuota sin interes \n 0. Salir sin comprar"));
-        switch (valor) {
-            case 12:
-                mensaje();
-                precioFinal(12, THIRTY);
-                break;
-            case 6:
-                mensaje();
-                precioFinal(6, TWENTY);
-                break;
-            case 3:
-                mensaje();
-                precioFinal(3, FIFTEEN);
-                break;
-            case 1:
-                mensaje();
-                precioFinal(1, 1);
-                break;
-            case 0:
-                console.log("Gracias por su visita");
-                break;
-        
-            default:
-                console.log("Seleccione una opcion correcta");
-                break;
-        }
-    } while (valor === null || (valor !== 0 && valor !== 12 && valor !== 6 && valor !== 3 && valor !== 1));
-}
-
-function menuCarrito() {
-    do {  
-        opcion = Number(prompt("Seleccione una opcion: \n 1. Visualizar carrito \n 2. Eliminar del carrito \n 3. Calcular cuotas \n 4. Volver a comprar \n 0. Finalizar y pagar"));
-        switch (opcion) {
-            case 1:
-carritoDom();
-                
-                break;
-            case 2:
-                eliminarCarrito();
-                break;
-            case 3:
-                calculoCuotas ();
-                break;
-            case 4:
-                compraStickers();
-                break;
-            case 0:
-                menuCuotas();
-                break;
-            default:
-                alert("Seleccione una opcion correcta");
-                break;
-        }
-    } while (opcion !== 0 || opcion === null)
-}
-
-alert("BIENVENIDOS A ANIME STICKERS 🤩")
-
-const contenedor = document.querySelector("#contenedor")
-const stickersDom = document.querySelector(".stickers")
-const detallesCompra = document.querySelector(".detallesCompra")
 
 function carritoDom () {
+    const stickersDom = document.querySelector(".stickers")
     arrayDeStickers.forEach(x => { 
         const item = document.createElement("div");
         item.innerHTML = `<h2>${x.nombre}</h2>
@@ -228,11 +59,97 @@ function carritoDom () {
 }
 
 
-const mensaje = () => {
+const mensajeDetelleCompra = () => {
+    const detallesCompra = document.querySelector(".detallesCompra")
     const titulo = document.createElement("div");
                 titulo.innerHTML = `DETALLES DE SU COMPRA`
                 detallesCompra.append(titulo);
 }
 
+function menuCuotas() {
+    do {
+        valor = Number (prompt("En cuantas cuotas lo queres pagar? \n 12 cuotas con recargo del 30% \n 6 cuotas con recargo del 20% \n 3 cuotas con recargo del 10% \n 1 cuota sin interes \n 0. Salir sin comprar"));
+        switch (valor) {
+            case 12:
+                mensajeDetelleCompra();
+                precioFinal(12, THIRTY);
+                break;
+            case 6:
+                mensajeDetelleCompra();
+                precioFinal(6, TWENTY);
+                break;
+            case 3:
+                mensajeDetelleCompra();
+                precioFinal(3, FIFTEEN);
+                break;
+            case 1:
+                mensajeDetelleCompra();
+                precioFinal(1, 1);
+                break;
+            case 0:
+                const resultado = document.createElement("div")
+                resultado.innerHTML = `GRACIAS POR SU VISITA`
+                resultado.className = "msjSalirSinComprar"
+                contenedor.append(resultado);
+                break;
+        
+            default:
+                console.log("Seleccione una opcion correcta");
+                break;
+        }
+    } while (valor === null || (valor !== 0 && valor !== 12 && valor !== 6 && valor !== 3 && valor !== 1));
+}
+
+
+function compraStickers () {
+    do {
+        opcion = Number (prompt("SELECCIONE SUS STICKERS:\n 10. Logo Mugiwara \n 9. Luffy \n 8. Zoro \n 7. Goku \n 6. Vegeta \n 5. Kid Buu \n 4. Tanjiro \n 3. Inosuke \n 2. Zenitsu \n 1. FINALIZAR"));
+        switch (opcion) {
+            case 10:
+                arrayDeStickers.push(mugiwara);              
+                break
+            case 9:
+                arrayDeStickers.push(luffy);
+                break
+    
+            case 8:
+                arrayDeStickers.push(zoro);
+                break
+            
+                case 7:
+                arrayDeStickers.push(goku);
+                break
+    
+                case 6:
+                arrayDeStickers.push(vegeta);
+                break
+    
+                case 5:
+                arrayDeStickers.push(kidBuu);
+                break
+    
+                case 4:
+                arrayDeStickers.push(tanjiro);
+                break
+    
+                case 3:
+                arrayDeStickers.push(inosuke);
+                break
+            
+                case 2:
+                arrayDeStickers.push(zenitsu);
+                break
+            
+                case 1:
+                break;
+                
+            default:
+                alert("Seleccione una opcion correcta");
+                break;
+        }
+    } while (opcion !== 1 || opcion === null);
+    }
+
 
 compraStickers();
+menuCuotas();
